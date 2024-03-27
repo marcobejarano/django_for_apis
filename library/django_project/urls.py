cbd2c9e5039485b0215f605dcceba8dbc0be4b25
Marco <m_uno_p@hotmail.com>
2024-03-26 22:13:51 -0500

    add Django REST Framework and created and API endpoint for our books

--- library/django_project/urls.py
+++ library/django_project/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include  # new
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("books.urls")),  # new
+    path("api/", include("apis.urls")),  # new
+    path("", include("books.urls")),
 ]
